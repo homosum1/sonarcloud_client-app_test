5c39249ff459e5dc353da09f1129b6a619f98940
Alex K <krab7153@gmail.com>
2024-04-28 23:23:48 +0200

    removed unused imports from eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,3 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
 export default {
   settings: {
     react: {
